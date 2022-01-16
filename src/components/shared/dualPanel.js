c0cf4b9e8109a83583b148a3d7459cac0095a393
Gabriel Segura <gabehd94@gmial.com>
2022-01-15 23:50:22 -0700

    added data to section

--- src/components/shared/dualPanel.js
+++ src/components/shared/dualPanel.js
@@ -8,16 +8,26 @@ export default styled.div`
     font-family: ${(props) => props.theme.font};
     display: flex;
     justify-content: center;
-    align-items: center;
+    flex-direction: column;
+    /* align-items: center; */
     margin: 100px 5.8vw;
     width: 30vw;
     height: 40vw;
     background-color: #fbc843;
     h1 {
-      font-size: 50px;
+      font-size: 3vw;
+      font-weight: 200;
+      margin: 0 10vw;
     }
     p {
-      font-size: 30px;
+      margin: 0 10vw 2vh 10vw;
+      color: #4b5168;
+      font-size: 0.8vw;
+    }
+    h2 {
+      margin: 0 10vw;
+      color: black;
+      font-size: 1.0vw;
     }
   }
 
@@ -41,21 +51,39 @@ export default styled.div`
       line-height: 6vw;
       letter-spacing: -1px;
       color: #2b3144;
-      @media screen and (max-width: 700px) {
-        width: 80vw;
-        line-height: 50px;
-        font-size: 10vw;
+      width: 40vw;
+
+      @media screen and (max-width: 820px) {
+        width: 50vw;
+        line-height: 60px;
+        font-size: 8vw;
+        margin: 0;
+      }
+      @media screen and (max-width: 600px) {
+        width: 90vw;
+        line-height: 9.9vw;
+        font-size: 11vw;
         margin: 0;
       }
     }
     p {
-      width: 34vw;
+      width: 30vw;
       font-family: "Roboto", sans-serif;
+      font-size: 1vw;
       font-weight: 900;
-      line-height: 25px;
+      line-height: 1.9vw;
+      @media screen and (max-width: 820px) {
+        width: 40vw;
+        font-size: 1.5vw;
+        line-height: 20px;
+        @media screen and (max-width: 600px) {
+          width: 80vw;
+          font-size: 3vw;
+        }
+      }
     }
   }
-  @media screen and (max-width: 700px) {
+  @media screen and (max-width: 600px) {
     flex-direction: column-reverse;
     width: 100%;
   }
--- src/sections/sectionTwo.js
+++ src/sections/sectionTwo.js
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import DualPanel from "../components/shared/dualPanel";
 import { ThemeProvider } from "styled-components";
-
 
 const theme = {
   font: "Bebas Neue",
@@ -25,33 +24,39 @@ export default function SectionTwo() {
     },
   ];
 
-
-
   const [currentCar, setCurrentCar] = useState(0);
   const handleAarowButton = () => {
-    const nextCar = 1 ;
+    const nextCar = 1;
     if (nextCar == 1) {
-        nextCar = 0
+      nextCar = 0;
     }
     setCurrentCar(nextCar);
-  }
+  };
   return (
     <ThemeProvider theme={theme}>
       <DualPanel>
-          <button onClick={handleAarowButton}>fdsfdsf</button>
+        {/* <button onClick={handleAarowButton}>fdsfdsf</button> */}
         <section>
           <h1>{carStats[currentCar].make}</h1>
-          {/* <p>{carStats[currentCar]}</p> */}
+          <p>{carStats[currentCar].model}</p>
+          <span>
+            <h2>{carStats[currentCar].mpg}</h2>
+          </span>
+          <span>
+            <h2>{carStats[currentCar].hp}</h2>
+          </span>
+          <span>
+            <h2>{carStats[currentCar].zeroSixty}</h2>
+          </span>
         </section>
 
-
         <main>
-            <h2>
-                SELECT A VEHICLE FROM YOUR HOME.
-            </h2>
-            <p>
-            Select from a wide range of luxury vehicles in our inventory.  Drive it for a month, trade it the next for something else you have always wanted to own.
-            </p>
+          <h2>SELECT A VEHICLE FROM YOUR PHONE.</h2>
+          <p>
+            Select from a wide range of luxury vehicles in our inventory. Drive
+            it for a month, trade it the next for something else you have always
+            wanted to own.
+          </p>
         </main>
       </DualPanel>
     </ThemeProvider>
